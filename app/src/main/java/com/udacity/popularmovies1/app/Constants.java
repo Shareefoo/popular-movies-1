d30024028255041f42a6db73916cdba9b12d705c
Shareef M. Sersawi <shareefworld.94@gmail.com>
2018-03-19 18:50:53 +0200

    - initial commit

--- app/src/main/java/com/udacity/popularmovies1/app/Constants.java
+++ app/src/main/java/com/udacity/popularmovies1/app/Constants.java
@@ -6,6 +6,6 @@ package com.udacity.popularmovies1.app;
 
 public class Constants {
 
-    public static final String API_KEY = "4a7d2c349f439f8ac8d00d0bff4b7e32";
+    public static final String API_KEY = "";
 
 }
